1da4310da245431f4591c1336054852a5cd0a945
ashleygrobler04 <31541911+ashleygrobler04@users.noreply.github.com>
2023-02-14 20:28:49 +0200

    Add files via upload

---
+++ bmi.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func calcBmi(height float64, weight float64) string {
+	res := fmt.Sprint(math.Round(weight / (height * height)))
+	return res
+}
+
+func main() {
+	result := calcBmi(1.82, 74.9)
+	fmt.Println(result)
+}
+//I hope this works.
